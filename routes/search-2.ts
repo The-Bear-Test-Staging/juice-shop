8a2d59c765bb40635b766fa066cc896aed3ec968
Cédric Fabianski <cedric@bearer.com>
2025-02-17 18:54:34 +0100

    Update search-2.ts

--- routes/search-2.ts
+++ routes/search-2.ts
@@ -15,15 +15,13 @@ class ErrorWithParent extends Error {
   parent: Error | undefined
 }
 
-// something here
-// just to be sure :)
-
 module.exports = function searchProducts2 () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    // this is same for now on
-    models.sequelize.query(`SELECT * FROM Products WHERE foo = 'foo'`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+
+    // hey you!
+    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
